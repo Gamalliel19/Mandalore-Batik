6585854dce391c5d2bd6ce055db9bd5d37dc46a2
Gamalliel Sharon <you@example.com>
2021-07-08 11:57:59 +0700

    adding responsive

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,21 @@
+const MobileMenu = () => {
+    return ( 
+        <div className="mobile-menu">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Produksi</a>
+                    </li>
+                    <li>
+                        <a href="/">Batik Daerah</a>
+                    </li>
+                    <li>
+                        <a href="/">Pengalaman</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+     );
+}
+ 
+export default MobileMenu;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import HomeContent from "../components/HomeContent"
+import MobileMenu from "../components/MobileMenu"
 
 const LandingPage = () => {
     return ( 
@@ -8,6 +9,7 @@ const LandingPage = () => {
             <Header />
             <HomeContent />
             <Footer />
+            <MobileMenu />
         </div>
      );
 }
